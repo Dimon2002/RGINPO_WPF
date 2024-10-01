d25b09293fec4dff795e30b8c58555afc1ecf576
Salnikov Dmitry <dima.salnikov.2014@inbox.ru>
2024-10-01 16:39:56 +0700

    Добавьте файлы проекта.

---
+++ Data.cs
@@ -0,0 +1,13 @@
+﻿namespace RGINPO_WPF;
+
+public class Data
+{
+    public double X { get; set; }
+    public double Y { get; set; }
+
+    public Data(double x, double y)
+    {
+        X = x;
+        Y = y;
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows;
+
+namespace RGINPO_WPF;
+
+public partial class MainWindow : Window
+{
+    private ObservableCollection<Data> dataSource =
+    [
+        new Data(0,0),
+        new Data(1,0),
+    ];
+
+    public MainWindow()
+    {
+        InitializeComponent();
+
+        dataGridView.ItemsSource = dataSource;
+    }
+    private void Button_Click(object sender, RoutedEventArgs e)
+    {
+        dataSource.Add(new Data(0, 0));
+
+    }
+}
